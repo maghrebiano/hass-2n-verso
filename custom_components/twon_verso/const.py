ad89d218333e4ae42e563faf242d86ec058442bf
maghrebiano <75951086+maghrebiano@users.noreply.github.com>
2024-09-24 21:55:38 +0200

    Update const.py

--- custom_components/twon_verso/const.py
+++ custom_components/twon_verso/const.py
@@ -1,9 +1,4 @@
-{
-  "domain": "2n_verso",
-  "name": "2N Verso",
-  "version": "1.0.0",
-  "documentation": "https://github.com/maghrebiano/hass-2n-verso",
-  "requirements": ["py2n==1.0.0"],
-  "dependencies": [],
-  "codeowners": ["@maghrebiano"]
-}
+DOMAIN = "2n_verso"
+CONF_HOST = "host"
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
