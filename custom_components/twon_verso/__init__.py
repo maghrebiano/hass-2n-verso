f075d6f4b454d69a0390369d36aa6b380d9fb7a2
maghrebiano <75951086+maghrebiano@users.noreply.github.com>
2024-09-24 22:54:39 +0200

    Update __init__.py

--- custom_components/twon_verso/__init__.py
+++ custom_components/twon_verso/__init__.py
@@ -3,12 +3,12 @@ import logging
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
-from py2n import Py2NDevice, Py2NConnectionData
 from .const import DOMAIN, CONF_HOST, CONF_USERNAME, CONF_PASSWORD
+from .api import VersoAPI
 
 _LOGGER = logging.getLogger(__name__)
 
-PLATFORMS = ["switch", "camera", "sensor"]
+PLATFORMS = ["sensor"]
 
 async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the 2N Verso component."""
@@ -21,19 +21,59 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     username = entry.data[CONF_USERNAME]
     password = entry.data[CONF_PASSWORD]
 
-    websession = async_get_clientsession(hass)
+    verso_api = VersoAPI(host, username, password)
     
     try:
-        verso = await Py2NDevice.create(
-            websession,
-            Py2NConnectionData(
-                host=host,
-                username=username,
-                password=password,
-            ),
-        )
-        hass.data[DOMAIN][entry.entry_id] = verso
-        hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+        system_info = await verso_api.get_system_info()
+        system_status = await verso_api.get_system_status()
+        switch_status = await verso_api.get_switch_status()
+        call_status = await verso_api.get_call_status()
+        camera_caps = await verso_api.get_camera_caps()
+        hass.data[DOMAIN][entry.entry_id] = {
+            "api": verso_api,
+            "system_info": system_info,
+            "system_status": system_status,
+            "switch_status": switch_status,
+            "call_status": call_status,
+            "camera_caps": camera_caps
+        }
+
+        async def handle_dial_call(call):
+            """Handle the service call."""
+            number = call.data.get("number")
+            users = call.data.get("users")
+            response = await verso_api.dial_call(number=number, users=users)
+            _LOGGER.info("Dial call response: %s", response)
+
+        async def handle_answer_call(call):
+            """Handle the service call."""
+            session = call.data.get("session")
+            response = await verso_api.answer_call(session=session)
+            _LOGGER.info("Answer call response: %s", response)
+
+        async def handle_hangup_call(call):
+            """Handle the service call."""
+            session = call.data.get("session")
+            reason = call.data.get("reason", "normal")
+            response = await verso_api.hangup_call(session=session, reason=reason)
+            _LOGGER.info("Hangup call response: %s", response)
+
+        async def handle_camera_snapshot(call):
+            """Handle the service call."""
+            width = call.data.get("width")
+            height = call.data.get("height")
+            source = call.data.get("source")
+            fps = call.data.get("fps")
+            time = call.data.get("time")
+            response = await verso_api.get_camera_snapshot(width=width, height=height, source=source, fps=fps, time=time)
+            _LOGGER.info("Camera snapshot response: %s", response)
+
+        hass.services.async_register(DOMAIN, "dial_call", handle_dial_call)
+        hass.services.async_register(DOMAIN, "answer_call", handle_answer_call)
+        hass.services.async_register(DOMAIN, "hangup_call", handle_hangup_call)
+        hass.services.async_register(DOMAIN, "camera_snapshot", handle_camera_snapshot)
+
+        await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     except Exception as err:
         _LOGGER.error("Error setting up 2N Verso: %s", err)
         return False
@@ -42,13 +82,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_forward_entry_unload(entry, "switch")
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
-    return unload_ok
 
-async def async_remove_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Remove a config entry."""
-    _LOGGER.info("Removing 2N Verso entry: %s", entry.entry_id)
-    if entry.entry_id in hass.data[DOMAIN]:
-        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
